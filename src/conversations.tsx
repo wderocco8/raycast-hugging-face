6de9c2c4015bf0bcfb3ea7213a6a2eff4705eafb
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-01-01 16:48:15 -0500

    added metadata to conversations detail

--- src/conversations.tsx
+++ src/conversations.tsx
@@ -1,18 +1,45 @@
-import { Icon, List } from "@raycast/api";
+import { Action, ActionPanel, List, useNavigation } from "@raycast/api";
 import { useConversations } from "./hooks/useConversations";
-import { formatRelativeTime } from "./utils/date/time";
+import { formatFullTime, formatRelativeTime } from "./utils/date/time";
+import { useQuestions } from "./hooks/useQuestions";
+import Chat from "./chat";
 
 export default function Conversations() {
   const { data: conversations, isLoading: isLoadingConversations } = useConversations();
-
+  const { getByConversation } = useQuestions();
+  const { push } = useNavigation();
 
   return (
-    <List>
+    <List isShowingDetail isLoading={isLoadingConversations}>
       {conversations.map((conversation) => (
         <List.Item
+          key={conversation.id}
           id={conversation.id}
           title={conversation.title}
-          accessories={[{ icon: Icon.Clock, text: formatRelativeTime(conversation.createdAt) }]}
+          accessories={[{ text: formatRelativeTime(conversation.createdAt) }]} // TODO: maybe remove?
+          detail={
+            <List.Item.Detail
+              isLoading={false} // TODO: replace this with individual loader
+              metadata={
+                <List.Item.Detail.Metadata>
+                  <List.Item.Detail.Metadata.Label title="Conversation Title" text={conversation.title} />
+                  <List.Item.Detail.Metadata.Label title="Date Created" text={formatFullTime(conversation.createdAt)} />
+                  <List.Item.Detail.Metadata.Separator />
+                  <List.Item.Detail.Metadata.Label title="Recent Questions" />
+                  {getByConversation(conversation.id)
+                    .slice(-5)
+                    .map((q) => (
+                      <List.Item.Detail.Metadata.Label title={`Q: ${q.prompt}`} key={q.id} />
+                    ))}
+                </List.Item.Detail.Metadata>
+              }
+            />
+          }
+          actions={
+            <ActionPanel>
+              <Action title="Push" onAction={() => push(<Chat conversationId={conversation.id} />)} />
+            </ActionPanel>
+          }
         />
       ))}
     </List>
