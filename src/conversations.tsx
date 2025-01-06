ed7e30d303e40000314b4e719b21cab0b3aa4231
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-01-06 11:16:55 -0500

    added empty state for conversations

--- src/conversations.tsx
+++ src/conversations.tsx
@@ -1,4 +1,4 @@
-import { Action, ActionPanel, Alert, confirmAlert, Keyboard, List, useNavigation } from "@raycast/api";
+import { Action, ActionPanel, Alert, confirmAlert, Keyboard, List, useNavigation, Image } from "@raycast/api";
 import { useConversations } from "./hooks/useConversations";
 import { formatFullTime, formatRelativeTime } from "./utils/date/time";
 import Chat from "./chat";
@@ -93,37 +93,53 @@ export default function Conversations() {
   );
 
   return (
-    <List isShowingDetail isLoading={isLoadingConversations} key={updateKey} actions={renderListActions()}>
-      {conversations.map((conversation) => (
-        <List.Item
-          key={conversation.id}
-          id={conversation.id}
-          title={conversation.title}
-          accessories={[{ text: formatRelativeTime(conversation.createdAt) }]} // TODO: maybe remove?
-          detail={
-            <List.Item.Detail
-              isLoading={isLoadingConversations}
-              // markdown={markdown(conversation)}
-              metadata={
-                <List.Item.Detail.Metadata>
-                  <List.Item.Detail.Metadata.Label title="Conversation Title" text={conversation.title} />
-                  <List.Item.Detail.Metadata.Label title="Date Created" text={formatFullTime(conversation.createdAt)} />
-                  <List.Item.Detail.Metadata.Label
-                    title="Question Count"
-                    text={conversation.questions?.length.toString() ?? "0"}
-                  />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="Recent Questions" />
-                  {conversation.questions?.map((q) => (
-                    <List.Item.Detail.Metadata.Label title={`Q: ${q.prompt}`} key={q.id} />
-                  ))}
-                </List.Item.Detail.Metadata>
-              }
-            />
-          }
-          actions={renderListItemActions(conversation)}
+    <List
+      isShowingDetail={conversations.length !== 0}
+      isLoading={isLoadingConversations}
+      key={updateKey}
+      actions={renderListActions()}
+    >
+      {conversations.length === 0 ? (
+        <List.EmptyView
+          icon={{ source: "no-conversations.jpeg", mask: Image.Mask.RoundedRectangle }}
+          title="No conversations yet"
+          description="Start making conversations with your AI wizard 🧙‍♂️"
         />
-      ))}
+      ) : (
+        conversations.map((conversation) => (
+          <List.Item
+            key={conversation.id}
+            id={conversation.id}
+            title={conversation.title}
+            accessories={[{ text: formatRelativeTime(conversation.createdAt) }]} // TODO: maybe remove?
+            detail={
+              <List.Item.Detail
+                isLoading={isLoadingConversations}
+                // markdown={markdown(conversation)}
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Label title="Conversation Title" text={conversation.title} />
+                    <List.Item.Detail.Metadata.Label
+                      title="Date Created"
+                      text={formatFullTime(conversation.createdAt)}
+                    />
+                    <List.Item.Detail.Metadata.Label
+                      title="Question Count"
+                      text={conversation.questions?.length.toString() ?? "0"}
+                    />
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label title="Recent Questions" />
+                    {conversation.questions?.map((q) => (
+                      <List.Item.Detail.Metadata.Label title={`Q: ${q.prompt}`} key={q.id} />
+                    ))}
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+            actions={renderListItemActions(conversation)}
+          />
+        ))
+      )}
     </List>
   );
 }
